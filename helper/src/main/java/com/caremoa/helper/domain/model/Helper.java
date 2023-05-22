af59a1a96f3d0289e43842032ad7bac8f2277945
Byounggwan Lee <lbg@sk.com>
2023-05-22 23:15:04 +0900

    PolishHandler 간소화 처리 Helper 작업 시작

---
+++ helper/src/main/java/com/caremoa/helper/domain/model/Helper.java
@@ -0,0 +1,72 @@
+package com.caremoa.helper.domain.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.caremoa.helper.domain.listener.HelperListener;
+import com.caremoa.helper.domain.model.vo.Address;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@EntityListeners({ HelperListener.class, AuditingEntityListener.class })
+@Table(name = "HELPER")
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC) // AccessLevel.PUBLIC
+@AllArgsConstructor
+@Builder
+public class Helper {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	Long id;
+	
+	@Column(name = "MEMBER_ID", nullable = false)
+	@Schema(description = "고객ID", nullable = false, defaultValue = "1")
+	Long memberId;
+	
+	@Column(name = "NAME", length = 50)
+	@Schema(description = "이름", nullable = true, defaultValue = "이병관")
+	private String name; // -- 이름
+
+	@Column(name = "NICKNAME", nullable = true, length = 40)
+	@Schema(description = "별명", nullable = true, defaultValue = "Daniel")
+	private String nickname; // -- 별명
+
+	private Address address; // -- 도/시, 군/구, 읍/동
+
+	@Column(name = "BIRTHDAY", nullable = true)
+	@Schema(description = "생년월일", nullable = true, defaultValue = "1968-12-25")
+	LocalDate birthday;
+	
+	@Column(name = "HELPER_SCORE", nullable = true)
+	@Schema(description = "도우미별점", nullable = true, defaultValue = "50")
+	Integer helperScore;
+	
+	@Column(name = "CAMCODER_AGREE", nullable = true)
+	@Schema(description = "근무중영상촬영동의여부", nullable = true, defaultValue = "TRUE")
+	Boolean camcoderAgree;
+	
+	@Column(name = "CAREER_PERIOD", nullable = true)
+	@Schema(description = "경력기간(개월)", nullable = true, defaultValue = "0")
+	Integer careerPeriod;
+	
+	@Column(name = "STATUS", nullable = true)
+	@Schema(description = "상태", nullable = true, defaultValue = "WRITING")
+	HelperStausType status;
+}
--- member/src/main/java/com/caremoa/member/adapter/PolicyHandler.java
+++ member/src/main/java/com/caremoa/member/adapter/PolicyHandler.java
@@ -1,5 +1,6 @@
 package com.caremoa.member.adapter;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
@@ -9,119 +10,98 @@ import org.springframework.messaging.support.ErrorMessage;
 import com.caremoa.member.domain.model.Member;
 import com.caremoa.member.domain.service.MemberService;
 import com.caremoa.member.exception.ApiException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
-* @packageName    : com.caremoa.member.adapter
-* @fileName       : PolicyHandler.java
-* @author         : 이병관
-* @date           : 2023.05.14
-* @description    : Cloud Stream 을 이용한 Pub/Sub 구현
-* ===========================================================
-* DATE              AUTHOR             NOTE
-* -----------------------------------------------------------
-* 2023.05.14        이병관       최초 생성
-*/
+ * @packageName : com.caremoa.member.adapter
+ * @fileName : PolicyHandler.java
+ * @author : 이병관
+ * @date : 2023.05.14
+ * @description : Cloud Stream 을 이용한 Pub/Sub 구현
+ *              =========================================================== DATE
+ *              AUTHOR NOTE
+ *              -----------------------------------------------------------
+ *              2023.05.14 이병관 최초 생성
+ */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class PolicyHandler {
 
 	private long errorOccur = 0;
-	private final MemberService memberService; 
+	private final MemberService memberService;
 	private final static int CONTRACT_SCORE = 1;
 	private final static int CLAIM_SCORE = -1;
 	private final static int REVIEW_SCORE = 2;
 
-    @Bean
-    Consumer<ContractCompleted> basicConsumer() {
-		return contractCompleted -> {
-			log.info( "{} : {}", contractCompleted.getEventType(), contractCompleted.validate());
-			log.info("teamUpdated 이벤트 수신 : {}", contractCompleted);
-		};
-	}
+	/**
+	 * @methodName : ReflectionScore
+	 * @date : 2023.05.19
+	 * @description : 점수반영
+	 * @param memberId
+	 */
+	private void ReflectionScore(int score, Long memberId) {
+		try {
+			log.debug("ReflectionScore {}, {}", score, memberId);
 
+			Member member = Member.builder().userScore(score).build();
 
-    @Bean
-    Consumer<ErrorMessage> KafkaErrorHandler() {
-		return e -> {
-	    	errorOccur++;
-	        log.error("에러 발생: {}, 횟수: {}", e, errorOccur);
-	    };
-	}
-    
-    /**
-     * @methodName    : ReflectionScore
-     * @date          : 2023.05.19
-     * @description   : 점수반영
-     * @param memberId
-    */
-    private void ReflectionScore(int score, Long memberId) {
-    	try {
-    		log.debug("ReflectionScore {}, {}", score, memberId);
-    		
-    		Member member = Member.builder().userScore(score).build();
-    		
-    		member = memberService.reflectionScore(member, memberId);
-    		
+			member = memberService.reflectionScore(member, memberId);
+
 			log.debug("점수반영 {}", member);
 		} catch (ApiException e) {
 			// TODO Auto-generated catch block
-			log.debug("{} : {}", e.getCode(), e.getMessage() );;
+			log.debug("{} : {}", e.getCode(), e.getMessage());
+			;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-    /**
-     * @methodName    : wheneverContractCompletedThenReflectionScore
-     * @date          : 2023.05.19
-     * @description   : 계약이 완료될 때마다 반영 점수
-     * @return
-    */
-    @Bean
-    Consumer<ContractCompleted>wheneverContractCompletedThenReflectionScore() {
-    	return contractCompleted -> {
-    		log.debug("Call contractCompleted : {}", contractCompleted.validate() );
-    		if ( contractCompleted.validate() ) {
-    			 ReflectionScore(CONTRACT_SCORE, contractCompleted.getMemberId());
-    		}
-    	};
-    }
-    
-    /**
-     * @methodName    : wheneverClaimCompletedThenReflectionScore
-     * @date          : 2023.05.19
-     * @description   : Claim이 완료될 때마다 반영 점수
-     * @return
-    */
-    @Bean
-    Consumer<ClaimCompleted>wheneverClaimCompletedThenReflectionScore() {
-    	return claimCompleted -> {
-    		log.debug("Call claimCompleted : {}", claimCompleted.validate() );
-    		if ( claimCompleted.validate() ) {
-    		     ReflectionScore(CLAIM_SCORE, claimCompleted.getMemberId());
-    		}
-    	};
-    }
-    
-    /**
-     * @methodName    : whenevereReviewMemberEvaluatedThenReflectionScore
-     * @date          : 2023.05.19
-     * @description   : 고객이 도우미로 부터 평가될 때마다 반영 점수
-     * @return
-    */
-    @Bean
-    Consumer<ReviewMemberEvaluated>whenevereReviewMemberEvaluatedThenReflectionScore() {
-    	return reviewMemberEvaluated -> {
-    		log.debug("Call reviewMemberEvaluated : {}", reviewMemberEvaluated.validate() );
-    		if ( reviewMemberEvaluated.validate() ) {
-    		     ReflectionScore(REVIEW_SCORE * ( reviewMemberEvaluated.getGreat() ? 1 : -1 )  , 
-    				        reviewMemberEvaluated.getMemberId());
-    		}
-    	};
-    }
+	@Bean
+	Consumer<Map<String, Object>> basicConsumer() {
+
+		return mapData -> {
+			ObjectMapper mapper = new ObjectMapper();
+			log.debug(mapData.toString());
+			
+			switch (mapData.get("eventType").toString()) {
+
+			case "ContractCompleted": // 계약이 완료될 때마다 반영 점수
+				ContractCompleted contractCompleted = mapper.convertValue(mapData, ContractCompleted.class);
+				log.debug("contractCompleted : {}", contractCompleted.toString());
+				ReflectionScore(CONTRACT_SCORE, contractCompleted.getMemberId());
+				break;
+
+			case "ClaimCompleted": // Claim이 완료될 때마다 반영 점수
+				ClaimCompleted claimCompleted = mapper.convertValue(mapData, ClaimCompleted.class);
+				log.debug("claimCompleted : {}", claimCompleted.toString());
+				ReflectionScore(CONTRACT_SCORE, claimCompleted.getMemberId());
+				break;
+
+			case "ReviewMemberEvaluated": // 고객이 도우미로 부터 평가될 때마다 반영 점수
+				ReviewMemberEvaluated reviewMemberEvaluated = mapper.convertValue(mapData, ReviewMemberEvaluated.class);
+				log.debug("reviewMemberEvaluated : {}", reviewMemberEvaluated.toString());
+				ReflectionScore(REVIEW_SCORE * (reviewMemberEvaluated.getGreat() ? 1 : -1),
+						reviewMemberEvaluated.getMemberId());
+				break;
+			
+			default: // 처리가 정의되지 않은 이벤트
+				log.debug("Undefined EventType : {}", mapData.get("eventType").toString());
+				break;
+			}
+		};
+	}
+
+	@Bean
+	Consumer<ErrorMessage> KafkaErrorHandler() {
+		return e -> {
+			errorOccur++;
+			log.error("에러 발생: {}, 횟수: {}", e, errorOccur);
+		};
+	}
 }
